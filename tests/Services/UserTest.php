8eb5937735503fdc3f53fcbaa6d60154509af897
badasswp <badasswpdev@gmail.com>
2024-12-26 13:58:35 +0100

    test: add test for user modification fails

--- tests/Services/UserTest.php
+++ tests/Services/UserTest.php
@@ -136,6 +136,26 @@ class UserTest extends TestCase {
 		$this->assertConditionsMet();
 	}
 
+	public function test_ping_on_user_modification_fails() {
+		$user_login = 'john@doe.com';
+
+		$user             = Mockery::mock( \WP_User::class )->makePartial();
+		$user->ID         = 1;
+		$user->user_login = 'john@doe.com';
+
+		\WP_Mock::userFunction( 'get_option' )
+			->with( 'ping_me_on_slack', [] )
+			->andReturn(
+				[
+					'enable_user' => false,
+				]
+			);
+
+		$this->user->ping_on_user_modification( $user->ID, [], [] );
+
+		$this->assertConditionsMet();
+	}
+
 	public function test_ping_on_user_modification() {
 		$user_login = 'john@doe.com';
 
