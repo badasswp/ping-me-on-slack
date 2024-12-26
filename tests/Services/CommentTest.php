b0c1e84036655a54b5e02fd1289b59c0db10bb3c
badasswp <badasswpdev@gmail.com>
2024-12-26 22:18:06 +0100

    test: change test name

--- tests/Services/CommentTest.php
+++ tests/Services/CommentTest.php
@@ -75,7 +75,7 @@ class CommentTest extends TestCase {
 		$this->assertConditionsMet();
 	}
 
-	public function test_ping_on_comment_status_change_passes() {
+	public function test_ping_on_comment_status_change_passes_on_approved() {
 		$comment = Mockery::mock( \WP_Comment::class )->makePartial();
 		$comment->shouldAllowMockingProtectedMethods();
 
