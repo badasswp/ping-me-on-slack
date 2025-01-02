4bba233837770d3f3f00c01328c9f2e0e912429e
badasswp <badasswpdev@gmail.com>
2025-01-02 16:27:48 +0100

    test: add more tests for Form class

--- tests/Admin/FormTest.php
+++ tests/Admin/FormTest.php
@@ -15,6 +15,12 @@ use PingMeOnSlack\Admin\Form;
  * @covers \PingMeOnSlack\Admin\Form::get_form_group
  * @covers \PingMeOnSlack\Admin\Form::get_form_group_body
  * @covers \PingMeOnSlack\Admin\Form::get_setting
+ * @covers \PingMeOnSlack\Admin\Form::get_form_control
+ * @covers \PingMeOnSlack\Admin\Form::get_text_control
+ * @covers \PingMeOnSlack\Admin\Form::get_checkbox_control
+ * @covers \PingMeOnSlack\Admin\Form::get_select_control
+ * @covers \PingMeOnSlack\Admin\Form::get_form_submit
+ * @covers \PingMeOnSlack\Admin\Form::get_form_notice
  */
 class FormTest extends TestCase {
 	public Form $form;
@@ -41,6 +47,20 @@ class FormTest extends TestCase {
 					'form_group_1',
 					'form_group_2',
 					'form_group_3',
+				],
+				'submit' => [
+					'heading' => 'Plugin Title',
+					'button'  => [
+						'name'  => 'button_name',
+						'label' => 'Button Label',
+					],
+					'nonce'   => [
+						'name'   => 'nonce_name',
+						'action' => 'nonce_action',
+					],
+				],
+				'notice' => [
+					'label' => 'Notice Label',
 				],
 			]
 		);
@@ -214,5 +234,281 @@ class FormTest extends TestCase {
 		$this->assertSame( 'Option 1', $this->form->get_setting( 'option_1' ) );
 		$this->assertSame( 'Option 2', $this->form->get_setting( 'option_2' ) );
 		$this->assertSame( 'Option 3', $this->form->get_setting( 'option_3' ) );
+	}
+
+	public function test_get_form_control_returns_text_control() {
+		$this->form->shouldReceive( 'get_text_control' )
+			->once()
+			->with(
+				[
+					'control'     => 'text',
+					'placeholder' => 'Text Placeholder',
+					'label'       => 'Text Label',
+					'summary'     => 'Text Summary',
+				],
+				'text_name'
+			)
+			->andReturn( 'Text Control' );
+
+		$control = $this->form->get_form_control(
+			[
+				'control'     => 'text',
+				'placeholder' => 'Text Placeholder',
+				'label'       => 'Text Label',
+				'summary'     => 'Text Summary',
+			],
+			'text_name'
+		);
+
+		$this->assertSame( 'Text Control', $control );
+	}
+
+	public function test_get_form_control_returns_select_control() {
+		$this->form->shouldReceive( 'get_select_control' )
+			->once()
+			->with(
+				[
+					'control'     => 'select',
+					'placeholder' => 'Select Placeholder',
+					'label'       => 'Select Label',
+					'summary'     => 'Select Summary',
+				],
+				'select_name'
+			)
+			->andReturn( 'Select Control' );
+
+		$control = $this->form->get_form_control(
+			[
+				'control'     => 'select',
+				'placeholder' => 'Select Placeholder',
+				'label'       => 'Select Label',
+				'summary'     => 'Select Summary',
+			],
+			'select_name'
+		);
+
+		$this->assertSame( 'Select Control', $control );
+	}
+
+	public function test_get_form_control_returns_checkbox_control() {
+		$this->form->shouldReceive( 'get_checkbox_control' )
+			->once()
+			->with(
+				[
+					'control'     => 'checkbox',
+					'placeholder' => 'Checkbox Placeholder',
+					'label'       => 'Checkbox Label',
+					'summary'     => 'Checkbox Summary',
+				],
+				'checkbox_name'
+			)
+			->andReturn( 'Checkbox Control' );
+
+		$control = $this->form->get_form_control(
+			[
+				'control'     => 'checkbox',
+				'placeholder' => 'Checkbox Placeholder',
+				'label'       => 'Checkbox Label',
+				'summary'     => 'Checkbox Summary',
+			],
+			'checkbox_name'
+		);
+
+		$this->assertSame( 'Checkbox Control', $control );
+	}
+
+	public function test_get_text_control() {
+		$this->form->shouldReceive( 'get_setting' )
+			->times( 1 )->with( 'text_name' )->andReturn( 'Text Name' );
+
+		$control = $this->form->get_text_control(
+			[
+				'control'     => 'text',
+				'placeholder' => 'Text Placeholder',
+				'label'       => 'Text Label',
+				'summary'     => 'Text Summary',
+			],
+			'text_name'
+		);
+
+		$this->assertSame(
+			'<input type="text" placeholder="Text Placeholder" value="Text Name" name="text_name"/>',
+			$control
+		);
+	}
+
+	public function test_get_checkbox_control() {
+		$this->form->shouldReceive( 'get_setting' )
+			->times( 1 )->with( 'checkbox_name' )->andReturn( 'on' );
+
+		$control = $this->form->get_checkbox_control(
+			[
+				'control'     => 'checkbox',
+				'placeholder' => 'Checkbox Placeholder',
+				'label'       => 'Checkbox Label',
+				'summary'     => 'Checkbox Summary',
+			],
+			'checkbox_name'
+		);
+
+		$this->assertSame(
+			'<input
+				name="checkbox_name"
+				type="checkbox"
+				checked
+			/>',
+			$control
+		);
+	}
+
+	public function test_get_select_control() {
+		$this->form->shouldReceive( 'get_setting' )
+			->times( 4 )->with( 'select_name' )->andReturn( 'selected_option' );
+
+		$control = $this->form->get_select_control(
+			[
+				'control'     => 'select',
+				'placeholder' => 'Select Placeholder',
+				'label'       => 'Select Label',
+				'summary'     => 'Select Summary',
+				'options'     => [
+					'not_selected_option_1' => 'Not Selected Option 1',
+					'not_selected_option_2' => 'Not Selected Option 2',
+					'selected_option'       => 'Selected Option',
+					'not_selected_option_3' => 'Not Selected Option 3',
+				],
+			],
+			'select_name'
+		);
+
+		$this->assertSame(
+			'<select name="select_name">
+				<option value="not_selected_option_1" >Not Selected Option 1</option><option value="not_selected_option_2" >Not Selected Option 2</option><option value="selected_option" selected>Selected Option</option><option value="not_selected_option_3" >Not Selected Option 3</option>
+			</select>',
+			$control
+		);
+	}
+
+	public function test_get_form_submit() {
+		\WP_Mock::userFunction( 'wp_nonce_field' )
+			->with( 'nonce_action', 'nonce_name', true, false )
+			->andReturn( '<input type="hidden" id="nonce_name" name="nonce_name" value="a8gkfhvzhi" />' );
+
+		$form_submit = $this->form->get_form_submit();
+
+		$this->assertSame(
+			'<div class="badasswp-form-group">
+				<p class="badasswp-form-group-heading">
+					<strong>Plugin Title</strong>
+				</p>
+				<p class="badasswp-form-group-heading">
+					<button name="button_name" type="submit" class="button button-primary">
+						<span>Button Label</span>
+					</button>
+				</p>
+				<input type="hidden" id="nonce_name" name="nonce_name" value="a8gkfhvzhi" />
+			</div>',
+			$form_submit
+		);
+	}
+
+	public function test_get_form_notice_bails_out_if_button_name_is_not_set() {
+		$_POST['nonce_name'] = 'nonce_action\/';
+
+		\WP_Mock::userFunction( 'wp_unslash' )
+			->andReturnUsing(
+				function ( $arg ) {
+					return rtrim( stripslashes( $arg ), '/' );
+				}
+			);
+
+		\WP_Mock::userFunction( 'sanitize_text_field' )
+			->andReturnUsing(
+				function ( $arg ) {
+					return $arg;
+				}
+			);
+
+		\WP_Mock::userFunction( 'wp_verify_nonce' )
+			->andReturnUsing(
+				function ( $arg1, $arg2 ) {
+					return $arg1 === $arg2;
+				}
+			);
+
+		$form_notice = $this->form->get_form_notice();
+
+		$this->assertSame(
+			'',
+			$form_notice
+		);
+	}
+
+	public function test_get_form_notice_bails_out_if_wp_verify_nonce_fails() {
+		$_POST['button_name'] = true;
+		$_POST['nonce_name']  = 'incorrect_action_name\/';
+
+		\WP_Mock::userFunction( 'wp_unslash' )
+			->andReturnUsing(
+				function ( $arg ) {
+					return rtrim( stripslashes( $arg ), '/' );
+				}
+			);
+
+		\WP_Mock::userFunction( 'sanitize_text_field' )
+			->andReturnUsing(
+				function ( $arg ) {
+					return $arg;
+				}
+			);
+
+		\WP_Mock::userFunction( 'wp_verify_nonce' )
+			->andReturnUsing(
+				function ( $arg1, $arg2 ) {
+					return $arg1 === $arg2;
+				}
+			);
+
+		$form_notice = $this->form->get_form_notice();
+
+		$this->assertSame(
+			'',
+			$form_notice
+		);
+	}
+
+	public function test_get_form_notice_passes() {
+		$_POST['button_name'] = true;
+		$_POST['nonce_name']  = 'nonce_action\/';
+
+		\WP_Mock::userFunction( 'wp_unslash' )
+			->andReturnUsing(
+				function ( $arg ) {
+					return rtrim( stripslashes( $arg ), '/' );
+				}
+			);
+
+		\WP_Mock::userFunction( 'sanitize_text_field' )
+			->andReturnUsing(
+				function ( $arg ) {
+					return $arg;
+				}
+			);
+
+		\WP_Mock::userFunction( 'wp_verify_nonce' )
+			->andReturnUsing(
+				function ( $arg1, $arg2 ) {
+					return $arg1 === $arg2;
+				}
+			);
+
+		$form_notice = $this->form->get_form_notice();
+
+		$this->assertSame(
+			'<div class="badasswp-form-notice">
+					<span>Notice Label</span>
+				</div>',
+			$form_notice
+		);
 	}
 }
