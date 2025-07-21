c4e57a959af78ccb10d2794b36219b1f52e5d56a
badasswp <badasswpdev@gmail.com>
2025-07-21 09:33:26 +0100

    refactor: use client args correctly

--- inc/Core/Client.php
+++ inc/Core/Client.php
@@ -15,13 +15,53 @@ use PingMeOnSlack\Interfaces\Dispatcher;
 
 class Client implements Dispatcher {
 	/**
+	 * Slack Client.
+	 *
+	 * @var SlackClient
+	 */
+	protected SlackClient $client;
+
+	/**
+	 * Constructor.
+	 *
+	 * @since 1.2.0
+	 */
+	public function __construct() {
+		$this->client = $this->get_client();
+	}
+
+	/**
+	 * Setter function.
+	 *
+	 * @since 1.2.0
+	 *
+	 * @param string $name   Class prop.
+	 * @param mixed  $params Slack params.
+	 */
+	public function __set( $name, $params ) {
+		if ( 'args' === $name ) {
+			$this->client = $this->get_client( $params );
+		}
+	}
+
+	/**
 	 * Get Slack Client.
 	 *
 	 * @since 1.1.3
+	 * @since 1.2.0 Introduce Slack Params.
 	 *
+	 * @params mixed[] $params Slack Params.
 	 * @return SlackClient
 	 */
-	protected function get_slack_client(): SlackClient {
+	protected function get_client( $params = [] ): SlackClient {
+		$args = wp_parse_args(
+			$params,
+			[
+				'channel'  => pmos_get_settings( 'channel' ),
+				'username' => pmos_get_settings( 'username' ),
+			]
+		);
+
 		return new SlackClient(
 			pmos_get_settings( 'webhook' ),
 			[
@@ -44,7 +84,7 @@ class Client implements Dispatcher {
 	 */
 	public function ping( $message ): void {
 		try {
-			$this->get_slack_client()->send( $message );
+			$this->client->send( $message );
 		} catch ( \Exception $e ) {
 			error_log(
 				sprintf(
